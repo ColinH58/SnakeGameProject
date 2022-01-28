074204fea7a20946c4d0966dc7c1bc9e4b755448
Colin Hoolwerf <choolwerf@gmail.com>
2022-01-28 17:09:40 -0600

    adding styles and HTML. also added specs to JS file

---
+++ snake.js
@@ -0,0 +1,14 @@
+/* 
+01 start the game by pressing a Start button
+02 use my arrow keys to change the direction of the snake
+03 have the snake grow correctly when it eats the apple
+04 have the game end if the snake tries to eat itself
+05 have the game end if the snake runs into a wall
+06 see how long my snake was when the game ended
+07 start the game over without having to reset the browser
+08 As a user playing the game I would be delighted if I:
+BONUS
+09 can set the difficulty (speed of snake)
+10 can keep track of my stats (maximum points, average points, etc.) between games
+*/
+
